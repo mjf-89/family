f115ff33ab48cdaecde8470584a7542a831423d6
Marco Jacopo Ferrarotti <marco.ferrarotti@gmail.com>
2018-10-08 15:57:55 +0200

    FILES: reorganized source-tree.

--- credits.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _ready():
-	get_node('RichTextLabel').bbcode_text = TranslationServer.tr('CREDITS').c_unescape()
-	get_node('RichTextLabel3').bbcode_text = TranslationServer.tr('CREDITS_THANKS').c_unescape()
-	get_node('RichTextLabel4').bbcode_text = TranslationServer.tr('CREDITS_FIN').c_unescape()
